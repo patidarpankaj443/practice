6017b3d2d35bd75db7007f8059bcacb35921a3ce
pankaj patidar <ppatidar812@gmail.com>
2021-05-29 12:18:19 +0530

    modified main java file

--- src/main/java/com/empManager/EmployeeManagerApplication.java
+++ src/main/java/com/empManager/EmployeeManagerApplication.java
@@ -2,7 +2,7 @@ package com.empManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Added code
 @SpringBootApplication
 public class EmployeeManagerApplication {
 
