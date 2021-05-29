4fcd01ea6f4e70be73f9ade0ec4786d187f80932
patidarpankaj443 <31337552+patidarpankaj443@users.noreply.github.com>
2021-05-29 12:36:39 +0530

    Update EmployeeManagerApplication.java
    added some comments by me

--- src/main/java/com/empManager/EmployeeManagerApplication.java
+++ src/main/java/com/empManager/EmployeeManagerApplication.java
@@ -2,7 +2,7 @@ package com.empManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Added code
+//Added code by pankaj patidar
 @SpringBootApplication
 public class EmployeeManagerApplication {
 
